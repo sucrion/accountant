3e44321a5a7108f0105cdaee0381ef87021f83c5
sucrion <curcubeuu@gmail.com>
2022-04-15 23:36:28 -0400

    js update

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+//MENU STUFF
 
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.display = "flex";
+  }
+  
+/* Close */
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+} 
+
+//HEADER
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("header").style.backgroundColor = "#84e6c7";
+    document.getElementById("headerLogo").style.visibility = "visible";
+  } else {
+    document.getElementById("header").style.backgroundColor = "inherit";
+    document.getElementById("headerLogo").style.visibility = "hidden";
+  }
+}
